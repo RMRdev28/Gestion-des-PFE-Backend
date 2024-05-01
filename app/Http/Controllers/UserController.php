6d621f47f158062ddb4102956dc3f244e753cc76
RMRdev28 <merouane.mezouari@etu.usthb.dz>
2024-05-01 20:58:39 +0100

    Users Modification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 
     public function index(){
         $users = User::all();
-        return response()->json();
+        return response()->json($users);
     }
 
 
