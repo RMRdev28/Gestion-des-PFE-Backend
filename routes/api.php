d33fb711fdced86863fc019fc89612239a0ac76c
RMRdev28 <merouane.mezouari@etu.usthb.dz>
2024-05-10 21:31:51 +0100

    Rdv

--- routes/api.php
+++ routes/api.php
@@ -36,8 +36,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/annonces/{annonce}', [AnnonceController::class, 'show']);// DISPLAY SINGLE
 
 
-    Route::post('/rdv', [SuiviPfeController::class, 'askForRdv']);
-    Route::post('/rdv/decision', [SuiviPfeController::class, 'acceptRdv']);//{date:dateTime}
+    Route::post('/rdvs', [SuiviPfeController::class, 'askForRdv']);
+    Route::get('/rdvs', [SuiviPfeController::class, 'askForRdv']);
+    Route::post('/rdvs/resume', [SuiviPfeController::class, 'askForRdv']);
+    Route::get('/rdvs/next', [SuiviPfeController::class, 'askForRdv']);
+    Route::post('/rdvs/decision', [SuiviPfeController::class, 'acceptRdv']);//{date:dateTime}
 
     Route::get('/notifications', [NotificationController::class, 'index']);// DISPLAY ALL NOTIFICATIONS
     Route::post('/notifications', [NotificationController::class, 'store']);// DISPLAY ALL NOTIFICATIONS
